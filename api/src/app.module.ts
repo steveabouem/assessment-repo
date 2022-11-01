474b21a1a42d973a76f556efdd6f2a4b3be66501
steveabouem <sabouem@c2.biz>
2022-10-31 23:03:55 -0400

    Unable to set db w migration in time. updt README

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Student } from './students/students.entity';
 import { StudentsModule } from './students/students.module';
 
+dotenv.config();
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'rootpass',
-      database: 'grades',
+      host: `${process.env.DB_HOST}`,
+      port: parseInt(process.env.DB_PORT),
+      username: `${process.env.DB_USER}`,
+      password: `${process.env.DB_PASSWORD}`,
+      database: `${process.env.DB_NAME}`,
       entities: [Student],
+      migrations: ['src/db/migrations'],
       synchronize: true,
     }),
     StudentsModule,
