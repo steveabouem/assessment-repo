306ff5002dc9d18205070293b9180b0e251e6a13
steveabouem <sabouem@c2.biz>
2022-10-31 20:41:25 -0400

    style updates

--- client/src/components/tabs/student-lookup/types.ts
+++ client/src/components/tabs/student-lookup/types.ts
@@ -1,6 +1,18 @@
-import { Student } from "../types";
-
 export interface StudentTableProps {
     students: Student[],
     isSearching: boolean,
+}
+
+export interface StudentSearchResults {
+    success: boolean;
+    message?: string;
+    data: Student[];
+}
+
+export interface Student {
+    id: number;
+    first_name: string;
+    last_name: string;
+    school_name: string;
+    grade: string;
 }
